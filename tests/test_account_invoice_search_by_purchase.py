11ef9f031da2915f6e84f093782138922a4f9e57
Raimon Esteve <raimon@nan-tic.com>
2017-10-04 14:54:36 +0200

    Add scenario rst test

--- tests/test_account_invoice_search_by_purchase.py
+++ tests/test_account_invoice_search_by_purchase.py
@@ -1,16 +1,24 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
-
-class TestCase(ModuleTestCase):
-    'Test module'
+class AccountInvoiceSearchByPurchaseTestCase(ModuleTestCase):
+    'Test Purchase module'
     module = 'account_invoice_search_by_purchase'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        AccountInvoiceSearchByPurchaseTestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_account_invoice_search_by_purchase.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='UTF-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
